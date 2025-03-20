fbab0dd3093aaafe435a4742e1e56e32521a5f86
Sergey Lavrent <91432388+hiimsergey@users.noreply.github.com>
2025-03-20 19:12:26 +0100

    Who needs icons anyways?

--- app/src/main/java/de/mminl/interscore_remoteend/MainActivity.kt
+++ app/src/main/java/de/mminl/interscore_remoteend/MainActivity.kt
@@ -1,5 +1,6 @@
 package de.mminl.interscore_remoteend
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -12,22 +13,15 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
 import androidx.compose.material3.BottomAppBar
-import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FilledTonalButton
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -36,19 +30,17 @@ import de.mminl.interscore_remoteend.ui.theme.InterscoreRemoteendTheme
 class MainActivity : ComponentActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
-		enableEdgeToEdge() // TODO CONSIDER
+		enableEdgeToEdge()
 		setContent { RemoteendApp() }
 	}
 }
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun RemoteendApp() {
-	var showMenu by remember { mutableStateOf(false) }
-
 	InterscoreRemoteendTheme {
 		Scaffold(
-			// TODO CONSIDER
 			modifier = Modifier.fillMaxSize(),
 			topBar = { TopAppBar(title = { Text("Interscore – Remoteend") }) },
 			bottomBar = {
@@ -56,7 +48,7 @@ fun RemoteendApp() {
 					Row(
 						modifier = Modifier
 							.fillMaxWidth()
-							.padding(horizontal = 16.dp),
+							.padding(16.dp),
 						horizontalArrangement = Arrangement.SpaceBetween,
 						verticalAlignment = Alignment.CenterVertically
 					) {
@@ -65,31 +57,26 @@ fun RemoteendApp() {
 					}
 				}
 			}
-		) { innerPadding ->
+		) { _ ->
 			Column(
-				modifier = Modifier
-					.fillMaxSize()
-					.padding(innerPadding),
+				modifier = Modifier.fillMaxSize(),
 				verticalArrangement = Arrangement.Center
 			) {
 				Surface(
 					modifier = Modifier
-						.padding(horizontal = 16.dp)
-						.wrapContentHeight(),
+						.wrapContentHeight()
+						.padding(horizontal = 16.dp),
 					shape = RoundedCornerShape(30.dp)
 				) {
 					Column(
-						modifier = Modifier
-							.wrapContentHeight(),
-						verticalArrangement = Arrangement.spacedBy(
-							10.dp, // TODO VALUE
-							Alignment.CenterVertically
-						)
+						modifier = Modifier.wrapContentHeight(),
+						verticalArrangement =
+							Arrangement.spacedBy(6.dp, Alignment.CenterVertically)
 					) {
-						ActionButton(text = "Toggle scoreboard", onClick = {})
-						ActionButton(text = "Toggle livetable", onClick = {})
-						ActionButton(text = "Toggle gameplan", onClick = {})
-						ActionButton(text = "Toggle gamestart", onClick = {})
+						ActionButton(text = "Toggle Scoreboard", onClick = {})
+						ActionButton(text = "Toggle Livetable", onClick = {})
+						ActionButton(text = "Toggle Gameplan", onClick = {})
+						ActionButton(text = "Toggle Gamestart", onClick = {})
 					}
 				}
 			}
@@ -100,20 +87,14 @@ fun RemoteendApp() {
 @Composable
 fun ActionButton(text: String, onClick: () -> Unit) {
 	Surface(
-		modifier = Modifier
-			.fillMaxWidth(),
+		modifier = Modifier.fillMaxWidth(),
 		color = MaterialTheme.colorScheme.surfaceContainerHigh,
 		onClick = onClick
 	) {
-		Row(
-			verticalAlignment = Alignment.CenterVertically,
-			horizontalArrangement = Arrangement.SpaceBetween
-		) {
-			Button(onClick = {}) { Text("TODO icon") }
-			Text(
-				modifier = Modifier.padding(30.dp),
-				text = text
-			)
-		}
+		Text(
+			modifier = Modifier.padding(30.dp),
+			style = MaterialTheme.typography.headlineLarge,
+			text = text
+		)
 	}
 }
