61a05ba5a43750effd3240c3e090b8d2e202c40f
Sergey Lavrent <91432388+hiimsergey@users.noreply.github.com>
2025-03-21 21:34:02 +0100

    Gave up for now

--- app/src/main/java/de/mminl/interscore_remoteend/MainActivity.kt
+++ app/src/main/java/de/mminl/interscore_remoteend/MainActivity.kt
@@ -11,12 +11,18 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.BasicAlertDialog
 import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FilledTonalButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Switch
@@ -25,11 +31,13 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
 import de.mminl.interscore_remoteend.ui.theme.InterscoreRemoteendTheme
 import okhttp3.OkHttpClient
@@ -47,6 +55,30 @@ class MainActivity : ComponentActivity() {
 	}
 }
 
+class WebSocketClient {
+	var message by remember { mutableStateOf("") }
+
+	fun new(port: Int) {
+
+	}
+}
+fun webSocketConnect(client: OkHttpClient, port: Int): WebSocket {
+	return client.newWebSocket(request, object : WebSocketListener() {
+		override fun onOpen(webSocket: WebSocket, response: Response) {
+			message = "Mit Port $port verbunden!"
+		}
+
+		override fun onMessage(webSocket: WebSocket, text: String) {
+			// TODO TEMP
+			message = text
+		}
+
+		override fun onFailure(webSocket: WebSocket, t: Throwable, response: Response?) {
+			message = "Verbindung gescheitert!"
+		}
+	})
+}
+
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -55,22 +87,13 @@ fun RemoteendApp() {
 	val client = remember { OkHttpClient() }
 	var webSocket: WebSocket? = null
 	var port = 8081
+	var changePort = remember { mutableStateOf(false) }
+	var textState = remember { mutableStateOf("") }
+	val keyboardController = LocalSoftwareKeyboardController.current
 
 	LaunchedEffect(Unit) {
-		val request = Request.Builder().url("ws://192.168.0.28:$port").build()
-		webSocket = client.newWebSocket(request, object : WebSocketListener() {
-			override fun onOpen(webSocket: WebSocket, response: Response) {
-				message = "Mit Port $port verbunden!"
-			}
-
-			override fun onMessage(webSocket: WebSocket, text: String) {
-				message = text
-			}
-
-			override fun onFailure(webSocket: WebSocket, t: Throwable, response: Response?) {
-				message = "Verbindung gescheitert!"
-			}
-		})
+		val request = Request.Builder().url("ws://192.0.0.1:$port").build()
+		webSocket = webSocketConnect(client, 8081)
 	}
 
 	InterscoreRemoteendTheme {
@@ -87,7 +110,9 @@ fun RemoteendApp() {
 						verticalAlignment = Alignment.CenterVertically
 					) {
 						Text(message)
-						FilledTonalButton(onClick = {}) { Text("Port ändern") }
+						FilledTonalButton(onClick = { changePort.value = true }) {
+							Text("Port ändern")
+						}
 					}
 				}
 			}
@@ -117,6 +142,38 @@ fun RemoteendApp() {
 					}
 				}
 			}
+
+			if (changePort.value) {
+				BasicAlertDialog(onDismissRequest = { changePort.value = false }) {
+					Card(
+						modifier = Modifier.size(300.dp, 100.dp),
+						shape = RoundedCornerShape(30.dp)
+					) {
+						Column(
+							modifier = Modifier.fillMaxSize(),
+							horizontalAlignment = Alignment.CenterHorizontally,
+							verticalArrangement = Arrangement.Center
+						) {
+							OutlinedTextField(
+								value = textState.value,
+								onValueChange = { it -> textState.value = it },
+								label = { Text("Server-Adresse") },
+								shape = RoundedCornerShape(30.dp),
+								singleLine = true,
+								keyboardActions = KeyboardActions(
+									onDone = {
+										keyboardController?.hide()
+										changePort.value = false
+									}
+								),
+								keyboardOptions = KeyboardOptions.Default.copy(
+									imeAction = ImeAction.Done
+								)
+							)
+						}
+					}
+				}
+			}
 		}
 	}
 }
@@ -128,16 +185,15 @@ fun ActionButton(text: String, onClick: (Boolean) -> Unit) {
 		color = MaterialTheme.colorScheme.surfaceContainerHigh
 	) {
 		Row(
+			modifier = Modifier.padding(24.dp),
 			horizontalArrangement = Arrangement.SpaceBetween,
 			verticalAlignment = Alignment.CenterVertically
 		) {
 			Text(
-				modifier = Modifier.padding(30.dp),
 				style = MaterialTheme.typography.headlineMedium,
 				text = text
 			)
 			Switch(
-				modifier = Modifier.padding(end = 30.dp),
 				checked = true,
 				onCheckedChange = onClick
 			)
