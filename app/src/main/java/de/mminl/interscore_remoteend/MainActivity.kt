4b5d675608080b9c592a87c0e9d43df391a511a7
Sergey Lavrent <91432388+hiimsergey@users.noreply.github.com>
2025-03-25 22:29:07 +0100

    Reworked layout

--- app/src/main/java/de/mminl/interscore_remoteend/MainActivity.kt
+++ app/src/main/java/de/mminl/interscore_remoteend/MainActivity.kt
@@ -1,6 +1,5 @@
 package de.mminl.interscore_remoteend
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -13,16 +12,14 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.KeyboardActions
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material3.BasicAlertDialog
-import androidx.compose.material3.BottomAppBar
-import androidx.compose.material3.Card
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.Send
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FilledTonalButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Switch
@@ -36,8 +33,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalSoftwareKeyboardController
-import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.unit.dp
 import de.mminl.interscore_remoteend.ui.theme.InterscoreRemoteendTheme
 import okhttp3.OkHttpClient
@@ -46,7 +42,43 @@ import okhttp3.Response
 import okhttp3.WebSocket
 import okhttp3.WebSocketListener
 import okio.ByteString
+import java.util.concurrent.TimeUnit
 
+class WebSocketClient(url: String) {
+	private val client = OkHttpClient.Builder()
+		.retryOnConnectionFailure(true)
+		.pingInterval(10, TimeUnit.SECONDS)
+		.build()
+	private val request = Request.Builder().url(url).build()
+
+	var webSocket: WebSocket? = null
+	var message: String by mutableStateOf("Verbindet...")
+
+	private val listener = object : WebSocketListener() {
+		override fun onOpen(webSocket: WebSocket, response: Response) {
+			message = "Mit Port 8081 verbunden!"
+		}
+
+		override fun onFailure(webSocket: WebSocket, t: Throwable, response: Response?) {
+			message = "Verbindung gescheitert!"
+		}
+
+		// TODO FINAL disconnect on destroying app
+	}
+
+	fun connect() {
+		webSocket = client.newWebSocket(request, listener)
+	}
+
+	// TODO
+	fun reconnect() {
+		webSocket?.cancel()
+		message = "Verbindet neu..." // TODO
+		connect()
+	}
+}
+
+// TODO FINAL dont make the phone sleep
 class MainActivity : ComponentActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
@@ -55,138 +87,60 @@ class MainActivity : ComponentActivity() {
 	}
 }
 
-@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun RemoteendApp() {
 	// TODO READ where to properly put this
-	var port = 8081
-	val client = remember { OkHttpClient() }
-	var webSocket: WebSocket? = null
-	var message by remember { mutableStateOf("") }
+	val wsc = WebSocketClient("ws://192.168.0.69:8081")
+	wsc.connect()
+	var statusText by remember { mutableStateOf(wsc.message) }
 
-	var changePort = remember { mutableStateOf(false) }
-
-	var textState = remember { mutableStateOf("") }
-	val keyboardController = LocalSoftwareKeyboardController.current
-
-	// TODO READ what this is for
-	LaunchedEffect(Unit) {
-		val request = Request.Builder().url("ws://192.168.0.69:$port").build()
-		// TODO READ how to REconnect
-		webSocket = client.newWebSocket(request, object : WebSocketListener() {
-			override fun onOpen(webSocket: WebSocket, response: Response) {
-				message = "Mit Port $port verbunden!"
-			}
-
-			override fun onMessage(webSocket: WebSocket, text: String) {
-				// TODO TEMP
-				message = text
-			}
-
-			override fun onFailure(webSocket: WebSocket, t: Throwable, response: Response?) {
-				message = "Verbindung gescheitert!"
-			}
-		})
-	}
+	LaunchedEffect(wsc.message) { statusText = wsc.message }
 
 	InterscoreRemoteendTheme {
 		Scaffold(
 			modifier = Modifier.fillMaxSize(),
-			topBar = { TopAppBar(title = { Text("Interscore – Remoteend") }) },
-			bottomBar = {
-				BottomAppBar {
-					Row(
-						modifier = Modifier
-							.fillMaxWidth()
-							.padding(16.dp),
-						horizontalArrangement = Arrangement.SpaceBetween,
-						verticalAlignment = Alignment.CenterVertically
-					) {
-						Text(message)
-						FilledTonalButton(onClick = { changePort.value = true }) {
-							Text("Port ändern")
-						}
-					}
-				}
-			}
-		) { _ ->
+			topBar = { TopAppBar(title = { Text("Interscore Remote") }) }
+		) { innerPadding ->
 			Column(
-				modifier = Modifier.fillMaxSize(),
-				verticalArrangement = Arrangement.Center
+				modifier = Modifier.fillMaxSize().padding(innerPadding),
+				verticalArrangement = Arrangement.SpaceEvenly
 			) {
-				Surface(
-					modifier = Modifier
-						.wrapContentHeight()
-						.padding(horizontal = 16.dp),
-					shape = RoundedCornerShape(30.dp)
+				Column(
+					modifier = Modifier.fillMaxWidth(),
+					horizontalAlignment = Alignment.CenterHorizontally,
+					verticalArrangement = Arrangement.spacedBy(16.dp, Alignment.CenterVertically)
 				) {
-					Column(
-						modifier = Modifier.wrapContentHeight(),
-						verticalArrangement =
-							Arrangement.spacedBy(6.dp, Alignment.CenterVertically)
-					) {
-						// TODO MOVE
-						var scoreboardHandle = remember { mutableStateOf(false) }
-						var gameplanHandle = remember { mutableStateOf(false) }
-						var livetableHandle = remember { mutableStateOf(false) }
-						var gamestartHandle = remember { mutableStateOf(false) }
-						var adHandle = remember { mutableStateOf(false) }
-
-						ActionButton(text = "Scoreboard", handle = scoreboardHandle.value, onClick = {
-							scoreboardHandle.value = !scoreboardHandle.value
-							webSocket?.send(ByteString.of(0))
-						})
-						ActionButton(text = "Gameplan", handle = gameplanHandle.value, onClick = {
-							gameplanHandle.value = !gameplanHandle.value
-							webSocket?.send(ByteString.of(1))
-						})
-						ActionButton(text = "Livetable", handle = livetableHandle.value, onClick = {
-							livetableHandle.value = !livetableHandle.value
-							webSocket?.send(ByteString.of(2))
-						})
-						ActionButton(text = "Gamestart", handle = gamestartHandle.value, onClick = {
-							gamestartHandle.value = !gamestartHandle.value
-							webSocket?.send(ByteString.of(3))
-						})
-						ActionButton(text = "Ad", handle = adHandle.value, onClick = {
-							adHandle.value = !adHandle.value
-							webSocket?.send(ByteString.of(4))
-						})
+					Text(
+						statusText,
+						style = MaterialTheme.typography.headlineSmall
+					)
+					Button(onClick = { wsc.reconnect() }) {
+						Text(
+							"Neu verbinden",
+							style = MaterialTheme.typography.bodyLarge
+						)
 					}
 				}
-			}
-
-			if (changePort.value) {
-				// TODO READ how to break away it into a Composable?
-				BasicAlertDialog(onDismissRequest = { changePort.value = false }) {
-					Card(
-						modifier = Modifier.size(300.dp, 100.dp),
-						shape = RoundedCornerShape(30.dp)
-					) {
-						Column(
-							modifier = Modifier.fillMaxSize(),
-							horizontalAlignment = Alignment.CenterHorizontally,
-							verticalArrangement = Arrangement.Center
-						) {
-							OutlinedTextField(
-								value = textState.value,
-								onValueChange = { it -> textState.value = it },
-								label = { Text("Server-Adresse") },
-								shape = RoundedCornerShape(30.dp),
-								singleLine = true,
-								keyboardActions = KeyboardActions(
-									onDone = {
-										keyboardController?.hide()
-										changePort.value = false
-									}
-								),
-								keyboardOptions = KeyboardOptions.Default.copy(
-									imeAction = ImeAction.Done
-								)
-							)
-						}
-					}
+				WidgetButtonColumn(wsc.webSocket)
+				Row(
+					modifier = Modifier.fillMaxWidth(),
+					horizontalArrangement = Arrangement.SpaceEvenly
+				) {
+					ActionButton(
+						Icons.AutoMirrored.Filled.Send,
+						labelOff = "Start stream",
+						labelOn = "Stop stream",
+						onOff = {},
+						onOn = {}
+					)
+					ActionButton(
+						Icons.Filled.Star,
+						labelOff = "Capture replay",
+						labelOn = "Abort replay",
+						onOff = {},
+						onOn = {}
+					)
 				}
 			}
 		}
@@ -194,13 +148,57 @@ fun RemoteendApp() {
 }
 
 @Composable
-fun ActionButton(text: String, handle: Boolean, onClick: (Boolean) -> Unit) {
+fun WidgetButtonColumn(webSocket: WebSocket?) {
+	Surface(
+		modifier = Modifier
+			.wrapContentHeight()
+			.padding(horizontal = 16.dp),
+		shape = MaterialTheme.shapes.extraLarge
+	) {
+		Column(
+			modifier = Modifier.wrapContentHeight(),
+			verticalArrangement =
+				Arrangement.spacedBy(6.dp, Alignment.CenterVertically)
+		) {
+			var scoreboardHandle by remember { mutableStateOf(false) }
+			var gameplanHandle by remember { mutableStateOf(false) }
+			var livetableHandle by remember { mutableStateOf(false) }
+			var gamestartHandle by remember { mutableStateOf(false) }
+			var adHandle by remember { mutableStateOf(false) }
+
+			WidgetButton("Scoreboard", scoreboardHandle) {
+				scoreboardHandle = !scoreboardHandle
+				webSocket?.send(ByteString.of(0))
+			}
+			WidgetButton("Gameplan", gameplanHandle) {
+				gameplanHandle = !gameplanHandle
+				webSocket?.send(ByteString.of(1))
+			}
+			WidgetButton("Livetable", livetableHandle) {
+				livetableHandle = !livetableHandle
+				webSocket?.send(ByteString.of(2))
+			}
+			WidgetButton("Gamestart", gamestartHandle) {
+				gamestartHandle = !gamestartHandle
+				webSocket?.send(ByteString.of(3))
+			}
+			WidgetButton("Ad", adHandle) {
+				adHandle = !adHandle
+				webSocket?.send(ByteString.of(4))
+			}
+		}
+	}
+}
+
+@Composable
+fun WidgetButton(text: String, handle: Boolean, onClick: () -> Unit) {
 	Surface(
 		modifier = Modifier.fillMaxWidth(),
-		color = MaterialTheme.colorScheme.surfaceContainerHigh
+		color = MaterialTheme.colorScheme.surfaceContainerHigh,
+		onClick = onClick
 	) {
 		Row(
-			modifier = Modifier.padding(24.dp),
+			modifier = Modifier.padding(horizontal = 24.dp, vertical = 18.dp),
 			horizontalArrangement = Arrangement.SpaceBetween,
 			verticalAlignment = Alignment.CenterVertically
 		) {
@@ -210,8 +208,77 @@ fun ActionButton(text: String, handle: Boolean, onClick: (Boolean) -> Unit) {
 			)
 			Switch(
 				checked = handle,
-				onCheckedChange = onClick
+				onCheckedChange = { onClick() }
 			)
 		}
+	}
+}
+
+@Composable
+fun ActionButton(
+	imageVector: ImageVector,
+	labelOff: String, labelOn: String,
+	onOff: () -> Unit, onOn: () -> Unit
+) {
+	var isClicked by remember { mutableStateOf(false) }
+	if (isClicked) ActionButtonOn(imageVector = imageVector, label = labelOn, onClick = onOn) {
+		isClicked = !isClicked
+	}
+	else ActionButtonOff(imageVector = imageVector, label = labelOff, onClick = onOff) {
+		isClicked = !isClicked
+	}
+}
+
+@Composable
+fun ActionButtonOn(imageVector: ImageVector, label: String, onClick: () -> Unit, switch: () -> Unit) {
+	Column(
+		horizontalAlignment = Alignment.CenterHorizontally,
+		verticalArrangement = Arrangement.spacedBy(16.dp)
+	) {
+		Button(
+			modifier = Modifier.size(80.dp),
+			onClick = {
+				onClick()
+				switch()
+			},
+			colors = ButtonDefaults.buttonColors(
+				containerColor = MaterialTheme.colorScheme.primary,
+				contentColor = MaterialTheme.colorScheme.surface
+			)
+		) {
+			Icon(
+				modifier = Modifier.size(32.dp),
+				imageVector = imageVector,
+				contentDescription = label
+			)
+		}
+		Text(label)
+	}
+}
+
+@Composable
+fun ActionButtonOff(imageVector: ImageVector, label: String, onClick: () -> Unit, switch: () -> Unit) {
+	Column(
+		horizontalAlignment = Alignment.CenterHorizontally,
+		verticalArrangement = Arrangement.spacedBy(16.dp)
+	) {
+		Button(
+			modifier = Modifier.size(80.dp),
+			onClick = {
+				onClick()
+				switch()
+			},
+			colors = ButtonDefaults.buttonColors(
+				containerColor = MaterialTheme.colorScheme.surfaceVariant,
+				contentColor = MaterialTheme.colorScheme.onSurface
+			)
+		) {
+			Icon(
+				modifier = Modifier.size(32.dp),
+				imageVector = imageVector,
+				contentDescription = label
+			)
+		}
+		Text(label)
 	}
 }
